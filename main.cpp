c8efb5fcaec208d9af8893119028ee31a28436c0
Luis96791 <luis.fajardozap@hotmail.com>
2016-02-29 15:11:53 -0600

    Resuelto

--- main.cpp
+++ main.cpp
@@ -8,37 +8,64 @@ using namespace std;
 //devuelve el primer elemento de "mi_pila"
 string getTope(stack<string> mi_pila)
 {
-    return "";
+    return mi_pila.top();
 }
 
 //devuelve la cantidad de elementos de "mi_pila"
 int getTamano(stack<string> mi_pila)
 {
-    return -1;
+    return mi_pila.size();
 }
 
 //devuelve true si "str" esta dentro de "mi_pila" de lo contrario devuelve false
 bool existeEnPila(stack<string> mi_pila, string str)
 {
+    while(!mi_pila.empty()){
+        if(mi_pila.top()==str){
+            return true;
+        }
+        mi_pila.pop();
+    }
     return false;
 }
 
 //devuelve el elemento mayor de "mi_pila"
 int getMayor(stack<int> mi_pila)
 {
-    return -1;
+    int mayor =0;
+    while(!mi_pila.empty()){
+        if(mi_pila.top()>mayor){
+            mayor = mi_pila.top();
+        }
+        mi_pila.pop();
+    }
+    return mayor;
 }
 
 //devuelve el elemento menor de "mi_pila"
 int getMenor(stack<int> mi_pila)
 {
-    return -1;
+    int menor = 9999;
+    while(!mi_pila.empty()){
+        if(mi_pila.top()<menor){
+            menor = mi_pila.top();
+        }
+        mi_pila.pop();
+    }
+    return menor;
 }
 
 //devuelve la cantidad de veces que se encuentra la letra 'a' minuscula en "mi_pila"
 int contarLetraA(stack<char> mi_pila)
 {
-    return -1;
+    int ac=0;
+    while(!mi_pila.empty()){
+        if(mi_pila.top()=='a' || mi_pila.top()=='A'){
+            ac++;
+        }
+        mi_pila.pop();
+    }
+    return ac;
 }
 
 int main ()
